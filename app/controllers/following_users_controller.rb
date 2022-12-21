2329bcb279d488bcba7da7e61e47bc2f1c904f44
DBN <dabeniao@users.noreply.github.com>
2022-12-21 16:55:04 +0800

    minor refactor

--- app/controllers/following_users_controller.rb
+++ app/controllers/following_users_controller.rb
@@ -1,12 +1,9 @@
 class FollowingUsersController < ApplicationController
-  def update
-    target_user = User.find_by(id: params[:id])
-    if target_user.nil?
-      render json: '', status: 404
-      return
-    end
 
-    if current_user.follow(target_user)
+  before_action :find_target_user, only: [:update, :destroy]
+
+  def update
+    if current_user.follow(@target_user)
       render json: '', status: 200
     else
       render json: '', status: 422
@@ -14,17 +11,18 @@ class FollowingUsersController < ApplicationController
   end
 
   def destroy
-    target_user = User.find_by(id: params[:id])
-    if target_user.nil?
-      render json: '', status: 404
-      return
-    end
-
-    if current_user.unfollow(target_user)
+    if current_user.unfollow(@target_user)
       render json: '', status: 200
     else
       render json: '', status: 422
     end
   end
 
+  private
+  def find_target_user
+    @target_user = User.find_by(id: params[:id])
+    if @target_user.nil?
+      render json: '', status: 404
+    end
+  end
 end
